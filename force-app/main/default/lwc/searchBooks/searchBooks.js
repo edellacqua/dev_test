a812595cac8298f347d6bb6aff487398376baf57
edellacqua <edellacqua@it-c02z32vqlvdm.homenet.telecomitalia.it>
2020-11-18 22:31:10 +0100

    Building lightning App page

---
+++ force-app/main/default/lwc/searchBooks/searchBooks.js
@@ -0,0 +1,30 @@
+import { LightningElement, track, wire } from 'lwc';
+import getLibraries from '@salesforce/apex/LibraryController.getLibraries';
+
+let i = 0;
+export default class SearchBooks extends LightningElement {
+    @track value = '';
+    @track error;
+    @track items = [];
+
+    @wire(getLibraries, {})
+    comboBoxItems({ error, data }) {
+        if (data) {
+            for(i=0; i<data.length; i++) {
+                this.items = [...this.items ,{value: data[i].Id , label: data[i].Name}];                                   
+            }
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.items = undefined;
+        }
+    }
+
+    get options(){
+        return this.items;      
+    }
+    
+    handleChange(event) {
+            this.value = event.detail.value;
+    }
+}
